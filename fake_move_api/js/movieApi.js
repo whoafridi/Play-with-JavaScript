dca66a26212ae1e08d38d980ed4cd84694fcae3b
Md. Rafiuzzaman Bhuiyan Afridi <afridirk10@gmail.com>
2021-09-09 21:11:19 +0600

    alert alongwith icon added

--- fake_move_api/js/movieApi.js
+++ fake_move_api/js/movieApi.js
@@ -9,8 +9,13 @@ const movieApi = () => {
     const searchText = searchField.value;
     // check search field empty or not
     if (searchField.value == ''){
-        result.innerHTML = `<div class="alert alert-light text-muted fw-bold" role="alert">
-        Please enter a valid movie-name.
+        result.innerHTML = `<div class="alert alert-light text-muted fw-bold d-flex align-items-center" role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg>
+        <div>
+            Please enter a valid movie name.
+        </div>
         </div>`
         searchField.value = '';
     }
@@ -18,9 +23,15 @@ const movieApi = () => {
     const url = `https://fake-movie-database-api.herokuapp.com/api?s=${searchText}`;
     // check search txt containes more than 3 letters or not
     if(searchText.length < 3){
-        result.innerHTML =  `<div class="alert alert-light text-muted fw-bold" role="alert">
-            Search term is too short, use three letters or more. <a href=${src} class="text-decoration-none">Learn more</a>
-                </div>`
+        result.innerHTML =  `
+        <div class="alert alert-light text-muted fw-bold d-flex align-items-center" role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg>
+            <div>
+                Search term is too short, use three letters or more. <a href=${src} class="text-decoration-none">Learn more</a>
+            </div>
+        </div>`
             searchField.value = '';
             searchResult.textContent = '';
     }else{
@@ -29,9 +40,14 @@ const movieApi = () => {
     .then(data => {
         // check movies are in api or not
         if (data.Search.length === 0) {
-            result.innerHTML =  `<div class="alert alert-light text-muted fw-bold" role="alert">
-                Movie not found. <a href=${src} class="text-decoration-none">Learn more</a>
-                </div>`
+            result.innerHTML =  `<div class="alert alert-light text-muted fw-bold d-flex align-items-center" role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg>
+        <div>
+            Movie not found. <a href=${src} class="text-decoration-none">Learn more</a>
+        </div>
+        </div>`
             searchResult.textContent = '';
         } else {
             displayResult(data);
