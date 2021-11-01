8139c702e7bedbce7d3a89d693eb606d4e35f171
Md. Rafiuzzaman Bhuiyan Afridi <afridirk10@gmail.com>
2021-11-01 22:13:27 +0600

    node practice updated

--- node-practice/index.js
+++ node-practice/index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const CORS = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.port || 5000;
 app.use(CORS());
 app.use(express.json());
 
+const { MongoClient } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j9yy4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+
+    // const database = client.db('sample_mflix');
+    // const movies = database.collection('movies');
+
+    // // Query for a movie that has the title 'Back to the Future'
+    // const query = { title: 'Back to the Future' };
+    // const movie = await movies.findOne(query);
+
+    // console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
 const users = [
     {id:1,name:"hello",email:"1@gmail.com"},
     {id:2,name:"hello",email:"1@gmail.com"},
@@ -38,3 +62,4 @@ app.get("/users/:id", (req,res) =>{
 });
 
 app.listen(port,()=>{console.log("listing from port", port)})
+
